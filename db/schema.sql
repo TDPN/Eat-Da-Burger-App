66d00d247538a7aff47c14fb163d285a5344bea7
Tuan Donald Nguyen <nguyentuand85@gmail.com>
2020-04-02 18:33:26 -0700

    pushing db

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burgers(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name varchar(30) NOT NULL,
+  devoured boolean not null default 0,
+  PRIMARY KEY (id)
+);
---
+++ db/seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) values ('Cheese Burger');
+INSERT INTO burgers (burger_name,devoured) values ('Chicken Sandwich',1);
+INSERT INTO burgers (burger_name) values ('Bacon and Cheese Burger');
